df6766e9d2b086be8737fbb5975a9d42df3a9c0c
Joe.cheng <joe.cheng@corp.to8to.com>
2024-02-23 12:09:22 +0800

    feat:ani

--- src/pages/subPackage/ai-goddness-home/component/animate-compare.vue
+++ src/pages/subPackage/ai-goddness-home/component/animate-compare.vue
@@ -76,16 +76,28 @@ export default class AnimateCompare extends Vue {
     }
     &.ani {
       .before {
-        animation: beforeAni 3s cubic-bezier(0.42, 0, 0.58, 1) infinite alternate;
-        @keyframes beforeAni {
+        // animation: beforeAni 3s cubic-bezier(0.87, 0.05, 0.39, 0.99) infinite alternate;
+        animation-name: anim1, anim2; /* 指定所有动画的名称 */
+        animation-duration: 3s; /* 设置动画持续时间 */
+        animation-timing-function: cubic-bezier(1, 0.16, 0.16, 1); /* 设置动画速度曲线 */
+        animation-delay: 0s; /* 设置第一个动画开始前的延迟时间 */
+        animation-iteration-count: infinite; /* 无限次重复播放动画 */
+        animation-direction: alternate; /* 正向播放动画 */
+        @keyframes anim1 {
           0% {
-            right: 5%;
-          }
-          50% {
             right: 90%;
           }
           100% {
             right: 5%;
+          }
+        }
+
+        @keyframes anim2 {
+          0% {
+            right: 5%;
+          }
+          100% {
+            right: 90%;
           }
         }
 
