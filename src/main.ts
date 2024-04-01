03a7679f125b70e096e1118d692769662d620427
Joe.cheng <joe.cheng@corp.to8to.com>
2024-04-01 16:44:44 +0800

    import testcom

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './uni.promisify.adaptor'
+// import warnSunBtn from '@chengzongxin/test-com'
+import TestCom from '@chengzongxin/test-com'
+import '@chengzongxin/test-com/lib/test-com.css'
+Vue.use(TestCom)
+
+// import warnSunBtn from 'warn_sun_btn_yjs_zt'
+// import 'warn_sun_btn_yjs_zt/lib/warn-sun-btn.css'
+
+// Vue.use(warnSunBtn)
 
 Vue.config.productionTip = false
 
-const app = new (typeof App === 'function' ? App : Vue.extend(Object.assign({ mpType: 'app' }, App)))
-app.$mount();
+const app = new (typeof App === 'function' ? App : Vue.extend(Object.assign({ mpType: 'app' }, App)))()
+app.$mount()
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,28 @@
 <template>
-  <view class="page">hahahahah</view>
+  <view class="page">
+    hahahahah
+
+    <p>----------------</p>
+    <test-com type="success">hhhh</test-com>
+    <p>----------------</p>
+    <test-com type="primary">hhhh</test-com>
+    <p>----------------</p>
+    <test-com type="test">hhhh</test-com>
+  </view>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+// import WarnSunBtn from '@chengzongxin/test-com/lib/warn-sun-btn.umd.min.js'
+// import OssUploader, { BIZ_ID_TYPE, DEFAULT_APPNAME } from '@to8to/oss-uploader/index.mp'
+// import TestCom from '@chengzongxin/test-com'
+// import TestCom from '@chengzongxin/test-com/lib/test-com.umd.min.js'
+// import TestCom from '@chengzongxin/test-com'
+
 @Component({
-  components: {},
+  components: {
+    // TestCom,
+  },
 })
 export default class Index extends Vue {}
 </script>
